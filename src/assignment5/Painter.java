0cfc8257cf3929e3428a6c17306eebf74868c660
GarrettCodester <custer.garrett@utexas.edu>
2016-11-08 02:05:11 -0600

    New Painter Class

--- src/assignment5/Painter.java
+++ src/assignment5/Painter.java
@@ -1,4 +1,4 @@
-/* CRITTERS Painter.java
+/* CRITTERS Critter.java
  * EE422C Project 5 submission by
  * Minh Van-Dinh
  * mv24772
@@ -10,41 +10,53 @@
  * Git URL: https://github.com/mvandinh/mv24772_gsc535_Lab4
  * Fall 2016
  */
+
 package assignment5;
 
 import javafx.scene.shape.Circle;
+import javafx.scene.shape.Polygon;
 import javafx.scene.shape.Rectangle;
 import javafx.scene.shape.Shape;
+import assignment4.Critter.CritterShape;
 
 public class Painter {
 
 	/*
 	 * Returns a square or a circle, according to shapeIndex
 	 */
-	static Shape getIcon(int shapeIndex) {
+	static Shape getIcon(CritterShape shapeIndex,javafx.scene.paint.Color fill, javafx.scene.paint.Color outline) {
 		Shape s = null;
-		int size = 100;
+		int size = 20;
 		
 		switch(shapeIndex) {
-		case 0: s = new Rectangle(size, size); 
-			s.setFill(javafx.scene.paint.Color.RED); break;
-		case 1: s = new Circle(size/2); 
-			s.setFill(javafx.scene.paint.Color.GREEN); break;
+		case SQUARE: s = new Rectangle(size, size); 
+			s.setFill(fill); break;
+		case CIRCLE: s = new Circle(size/2); 
+			s.setFill(fill); break;
+		case TRIANGLE: s = new Polygon();
+		((Polygon) s).getPoints().addAll(new Double[]{
+			    0.0, 0.0,
+			    (double) size, (double) (size/2),
+			    (double) size, 0.0 });
+			s.setFill(fill);
+		case DIAMOND: s = new Polygon();
+		((Polygon) s).getPoints().addAll(new Double[]{
+				(double) (size/2), 0.0,
+			    (double) size, (double) (size/2),
+			    (double) (size/2), (double) size,
+			    0.0, (double) (size/2) });
+			s.setFill(fill);
+		case STAR: s = new Polygon();
+		((Polygon) s).getPoints().addAll(new Double[]{
+				(double) (size/5), (double) size,
+			    (double) (size/2), (double) 0.0,
+			    (double) (4*size/5), (double) size,
+			    0.0, (double) (2*size/5),
+			    (double) size, (double) (2*size/5)});
+			s.setFill(fill);
 		}
 		// set the outline of the shape
-		s.setStroke(javafx.scene.paint.Color.BLUE); // outline
+		s.setStroke(outline); // outline
 		return s;
-	}
-	
-	/*
-	 * Paints the shape on a grid.
-	 */
-	public static void paint() {
-		Main.grid.getChildren().clear(); // clean up grid.
-		for (int i = 0; i <= 1; i++) {
-			Shape s = getIcon(i);	// convert the index to an icon.
-			Main.grid.add(s, i, i); // add the shape to the grid.
-		}
-		
 	}
 }
